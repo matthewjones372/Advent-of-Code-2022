9af62a2503d9ce5702de1a7d79ee90c1b6d60ef5
Matthew Jones <matthewjones372@gmail.com>
2022-12-01 12:53:41 +0000

    Day 1

--- src/main/scala/day1/Day1.scala
+++ src/main/scala/day1/Day1.scala
@@ -1,20 +1,23 @@
 package day1
 
 import scala.io.Source
+import scala.util.Using
 
+extension (seq: Seq[Int])
+  def top(n: Int): Seq[Int] =
+    seq.sorted.takeRight(n)
 
 object Day1 extends App:
-  val elfWeights = Source
-    .fromResource("day1_input.txt")
-    .getLines()
-    .map(_.trim)
-    .mkString("\n")
-    .split("\n\n")
-    .map(_.split("\n").map(_.toInt).sum)
 
+  val elfWeights = Using.resource(Source.fromResource("day1_input.txt"))(
+    _.getLines()
+      .mkString("\n")
+      .split("\n\n")
+      .map(_.split("\n").map(_.toInt).sum)
+  )
 
   val solution1 = elfWeights.max
-  val solution2 = elfWeights.sorted(Ordering.Int.reverse).take(3).sum
+  val solution2 = elfWeights.top(n = 3).sum
 
   println(s"Part 1: $solution1")
   println(s"Part 2: $solution2")
