a61c5b7211a05f27d8eb4fc076bd40f08b0de60f
Matthew Jones <matthewjones372@gmail.com>
2022-12-03 09:15:58 +0000

    Day 3

---
+++ src/main/scala/day3/Day3.scala
@@ -0,0 +1,32 @@
+package day3
+
+import scala.io.Source
+import scala.util.Using
+
+object Priority:
+  private val alphas =
+    ('a' to 'z').zipWithIndex.map { case (c, i) => (c, i + 1) }.toMap
+      ++ ('A' to 'Z').zipWithIndex.map { case (c, i) => (c, i + 27) }.toMap
+
+  def of(char: Char): Int = alphas.getOrElse(char, 0)
+
+@main def main =
+  val sacks = Using.resource(
+    Source
+      .fromResource("day3_input.txt")
+  ) {
+    _.getLines().toList
+  }
+
+  val solution1 = sacks.flatMap { sack =>
+    val (left, right) = sack.splitAt(sack.length / 2)
+    (left intersect right).distinct.map(Priority.of)
+  }.sum
+
+  val solution2 = sacks
+    .grouped(3)
+    .flatMap(_.reduce(_ intersect _).headOption.map(Priority.of))
+    .sum
+
+  println(s"${Console.RED} Part 1:${Console.GREEN} $solution1${Console.RESET}")
+  println(s"${Console.RED} Part 2:${Console.GREEN} $solution2${Console.RESET}")
