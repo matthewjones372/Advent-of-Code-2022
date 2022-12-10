f38aadf9d98410c6a2ec163b7463133542b6f05c
Matthew Jones <matthewjones372@gmail.com>
2022-12-10 17:03:20 +0000

    Day 10 (tidy)

--- src/main/scala/day10/Day10.scala
+++ src/main/scala/day10/Day10.scala
@@ -1,14 +1,12 @@
 package day10
 
-import zio.Chunk
+import zio.*
 
 import scala.io.Source
 import scala.util.Using
-import zio.*
-import scala.Console as SConsole
 
 sealed trait Instruction
-object Instruction {
+object Instruction:
   case object NOOP                  extends Instruction
   final case class AddX(value: Int) extends Instruction
 
@@ -16,7 +14,6 @@ object Instruction {
     instruction match
       case s"noop"        => NOOP
       case s"addx $value" => AddX(value = value.toInt)
-}
 
 object Day10 extends ZIOAppDefault:
   val instructions =
@@ -24,36 +21,40 @@ object Day10 extends ZIOAppDefault:
       Chunk.fromIterator(l.getLines().map(Instruction.from))
     }
 
-  def countInstructions(ref: Ref[Chunk[Int]]) =
-    ZIO.foreach(instructions) {
-      case Instruction.NOOP =>
-        ref.getAndUpdate(acc => acc :+ acc.last)
-      case Instruction.AddX(value) =>
-        ref.getAndUpdate(acc => acc ++: Chunk(acc.last, acc.last + value))
+  def countInstructions: UIO[Ref[Chunk[Int]]] =
+    Ref.make(Chunk(1)).flatMap { ref =>
+      ZIO
+        .foreach(instructions) {
+          case Instruction.NOOP =>
+            ref.getAndUpdate(acc => acc :+ acc.last)
+          case Instruction.AddX(value) =>
+            ref.getAndUpdate(acc => acc ++: Chunk(acc.last, acc.last + value))
+        }
+        .as(ref)
     }
 
-  def solution1(ref: Ref[Chunk[Int]]) =
+  def countRegisters(ref: Ref[Chunk[Int]]) =
     ref.get.map {
-      _.zipWithIndex.collect {
-        case (register, cycle) if (cycle - 19) % 40 == 0 =>
-          register * (cycle + 1)
+      _.zipWithIndex.map { case (value, index) => (value, index + 1) }.collect {
+        case (register, cycle) if (cycle - 20) % 40 == 0 =>
+          register * cycle
       }.sum
     }
 
-  def solution2(ref: Ref[Chunk[Int]]) = ref.get.map(
-    _.grouped(40)
-      .map(_.zipWithIndex.map { case (column, index) =>
-        if (index - column).abs <= 1 then s"${SConsole.GREEN}🎅" else s"${SConsole.GREEN}#"
-      }.mkString("\t"))
-      .mkString("\n")
-  )
+  def decode(ref: Ref[Chunk[Int]]) =
+    ref.get.map(
+      _.grouped(40)
+        .map(_.zipWithIndex.map { case (column, index) =>
+          if (index - column).abs <= 1 then "🎅" else "🎄"
+        }.mkString("\t"))
+        .mkString("\n")
+    )
 
   def run = (for {
-    ref <- Ref.make(Chunk(1))
-    _   <- countInstructions(ref)
-    r1  <- solution1(ref)
-    r2  <- solution2(ref)
+    ref <- countInstructions
+    r1  <- countRegisters(ref)
+    r2  <- decode(ref)
     _   <- Console.printLine(s"Solution 1: $r1")
     _   <- Console.printLine("Solution 2")
     _   <- Console.printLine(r2)
-  } yield r1)
+  } yield (r1, r2)).timed.tap { case (time, (_, _)) => Console.printLine(s"${time}ms") }
--- src/main/scala/day8/Day8.scala
+++ src/main/scala/day8/Day8.scala
@@ -8,7 +8,7 @@ object Day8 extends ZIOAppDefault:
   val data = Chunk.fromIterator(io.Source.fromResource("day8_input.txt").getLines())
   val grid = Grid(data)
 
-  val run = for {
+  val run = (for {
     s1 <- grid.visibleTrees.timed
     s2 <- grid.maxView.timed
     _ <-
@@ -19,4 +19,4 @@ object Day8 extends ZIOAppDefault:
       Console.printLine(
         s"$RED Solution 2:$GREEN ${s2._2}\t$BLUE Execution Time: $YELLOW${s2._1.toMillis}ms$RESET"
       )
-  } yield ()
+  } yield ())
