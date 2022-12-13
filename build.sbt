a4f663adf3ff794dfd5cb48e1d28d7a7ef41abce
Matthew Jones <matthew.jones@pirum.com>
2022-12-13 15:25:41 +0000

    Day 12: fix

--- build.sbt
+++ build.sbt
@@ -11,7 +11,8 @@ libraryDependencies ++= Seq(
   "dev.zio" %% "zio"         % "2.0.4",
   "dev.zio" %% "zio-streams" % "2.0.4",
   "dev.zio" %% "zio-prelude" % "1.0.0-RC16",
-  "dev.zio" %% "zio-nio"     % "2.0.0"
+  "dev.zio" %% "zio-nio"     % "2.0.0",
+  "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1"
 )
 
 ThisBuild / semanticdbEnabled := false
--- src/main/scala/day12/Day12.scala
+++ src/main/scala/day12/Day12.scala
@@ -10,28 +10,30 @@ type Points = Map[Point, Char]
 
 final class Grid(points: Points):
   def bfs(start: Point, target: Point) =
+    val targetChar = points(target)
+
     @tailrec
     def loop(remaining: Chunk[Point], costMap: Map[Point, Int]): Int =
       if remaining.isEmpty then -1
       else {
-        val point = remaining.head
-        if point == target then costMap(point)
+        val currentPoint = remaining.head
+        if points(currentPoint) == targetChar then costMap(currentPoint)
         else {
           val (moreRemaining, newCostMap) =
-            neighborsOf(point).filter(points.contains).foldLeft((remaining, costMap)) {
-              case ((remaining, costMap), neighbor) =>
-                if isBlocked(point, neighbor) && !costMap.contains(neighbor) then {
-                  (remaining :+ neighbor, costMap.updated(neighbor, costMap(point) + 1))
-                } else (remaining, costMap)
-            }
-
+            neighborsOf(currentPoint)
+              .filter(neighbor => points.contains(neighbor) && !costMap.contains(neighbor))
+              .foldLeft((remaining, costMap)) { case ((remaining, costMap), neighbor) =>
+                if notBlocked(currentPoint, neighbor) then
+                  (remaining :+ neighbor, costMap.updated(neighbor, costMap(currentPoint) + 1))
+                else (remaining, costMap)
+              }
           loop(moreRemaining.tail, newCostMap)
         }
       }
 
     loop(remaining = Chunk(start), costMap = Map(start -> 0))
 
-  private def isBlocked(current: Point, other: Point): Boolean =
+  private def notBlocked(current: Point, other: Point): Boolean =
     def elevation(point: Point) = points(point) match
       case 'S'   => 'a'
       case 'E'   => 'z'
@@ -60,8 +62,8 @@ object PointParser:
   val points    = PointParser.parse(data)
   val grid      = Grid(points)
   val start     = points.map(_.swap)('E')
-  val solution1 = grid.bfs(start, points.map(_.swap)('S'))
-  val solution2 = grid.bfs(start, points.map(_.swap)('a'))
+  val solution1 = grid.bfs(start = start, target = points.map(_.swap)('S'))
+  val solution2 = grid.bfs(start = start, target = points.map(_.swap)('a'))
 
   println(s"${Console.RED} Part 1:${Console.GREEN} $solution1${Console.RESET}")
   println(s"${Console.RED} Part 2:${Console.GREEN} $solution2${Console.RESET}")
